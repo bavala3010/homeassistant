6dce73681d70b6d3c257f79b35b653abc1986642
bavala3010 <bartland@gmail.com>
2023-02-13 22:35:32 +0100

    veel kleine updates

---
+++ mobile_vikings.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+cd /home/bart/Python_BVL/PROD
+python3 mv_anita.py
+python3 mv_madouce.py
+python3 mv_alunya.py
+python3 mv_anita_to_HA.py
+python3 mv_madouce_to_HA.py
+python3 mv_alunya_to_HA.py
